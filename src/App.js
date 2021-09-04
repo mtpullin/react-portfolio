c2f47fb24ae188d39e3332be849b876f47f4ac0d
mtpullin <mtpullin@gmail.com>
2021-09-04 10:35:27 -0400

    adding case switching

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React, {useState} from 'react'
 import About from './components/About'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import ContactForm from './components/Contact'
 
 function App() {
+  const [currentPage, handlePageChange]= useState('Home')
+  const renderPage = () => {
+    switch(currentPage){
+      case 'About':
+        return<About />;
+      case 'Contact':
+        return<ContactForm />
+      // case 'Projects'
+      //   return<Projects />
+    }
+  }
   return (
     <div>
       <Header>
