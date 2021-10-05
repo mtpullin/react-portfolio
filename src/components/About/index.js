4cc569eb8896e440ee9692fd6272845f69cb1e61
mtpullin <mtpullin@gmail.com>
2021-10-05 08:06:34 -0400

    added brand statement to about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,9 +10,12 @@ function About() {
                     <img src={personalImage}  style={{width:"60%"}} alt="me" />
                     </div>
                     <div>
-                    My name is Mitchell Pullin, I am a newly budding coder learning the ropes. I made this website
-                    using react to help improve my portfolio page. I enjoy playing video games as well as 
-                    watching movies and anime.
+                    My name is Mitchell Pullin, I am a Full-Stack web developer leveraging restaurant background to build a more intuitive user experience on the web. 
+                    Recently earned a certificate in full stack development from the University of Central Florida Coding Bootcamp, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. 
+                    Known as a problem solver and hard worker, passionate about developing apps, and moving into a new field. With each project, my aim is to best engage my audience for an impactful user experience. 
+                    I applied aspects of UX and agile development in a recent project. I worked on a team of three to develop a single-page MERN app resembling a satirical myspace/facebook styling to allow people who believe in flat-earth to befriend one another. 
+                    I’m excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web. 
+
                     </div>
                    
                 </div>
